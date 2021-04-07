70a81e9f796aaed7d257aa1a1f43fed1dc0c7845
nach022 <68956665+nach022@users.noreply.github.com>
2021-04-07 06:30:11 -0300

    assets correction

--- front-end/src/app/block-template.component.ts
+++ front-end/src/app/block-template.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="block-ui-spinner">
 			<div class="block-ui-template" height="100%">
-        <img src="/assets/img/loading.svg" alt="Loading..." style="margin-left: auto; margin-right: auto; display: block;" height="150" width="150">
+        <img src="./assets/img/loading.svg" alt="Loading..." style="margin-left: auto; margin-right: auto; display: block;" height="150" width="150">
 			</div>
 			<div class="message ng-star-inserted" style="position: relative; top: 25px;"> {{ message }} </div>
   `
