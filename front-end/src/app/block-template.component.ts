3b7b98347e7d5f65d7611ac81ed709532af42944
nach022 <68956665+nach022@users.noreply.github.com>
2023-05-12 14:02:28 -0300

    nuevo logo en cursor y en reportes

--- front-end/src/app/block-template.component.ts
+++ front-end/src/app/block-template.component.ts
@@ -3,11 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'block-temp',
   template: `
-    <div class="block-ui-spinner">
-			<div class="block-ui-template" height="100vh">
-        <img src="./assets/img/loading.svg" alt="Loading..." style="margin-left: auto; margin-right: auto; display: block;" height="150" width="150">
+    <div class="block-ui-spinner" style="position: absolute;  top: 45%;  left: 50%;  transform: translate(-50%, -50%);">
+			<div class="block-ui-template" style="text-align: center;">
+      <img
+          src="/assets/img/iso.svg"
+          alt="Loading..."
+          height="115"
+        >
 			</div>
-			<div class="message ng-star-inserted" style="position: relative; top: 25px;"> {{ message }} </div>
+			<div class="message ng-star-inserted" style="text-align: center; position: relative; top: 60px;"> {{ message }} </div>
+    </div>
   `
 })
 export class BlockTemplateComponent {
